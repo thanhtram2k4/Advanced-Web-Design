5d74ab5015234d83b5b8a482576943e2584ccee5
thanhtram2k4 <thanhtram2k4@gmail.com>
2024-11-21 19:49:35 +0700

    HW12

---
+++ FoodOrder-API/models/category.model.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');	
+
+const CategorySchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    img: { type: String, required: true }
+});
+
+module.exports =  mongoose.model('categories', CategorySchema);
